879933db0fdbd00e58f1b9a75a07a7500e709e50
Lukas <69743171+quick007@users.noreply.github.com>
2021-10-11 21:58:09 -0700

    see if netlify's example form works

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { LocLink } from "./misc";
 import nightwind from "nightwind/helper";
 import React, { useState, useEffect } from "react";
 import { SparklesIcon, SunIcon, XIcon } from "@heroicons/react/outline"
+import ContactForm from "./contact"
 
 
 
@@ -102,60 +103,7 @@ export default function navbar(props) {
 								<h2 className="text-sm text-gray-500 text-center mb-6 lg:px-36">
 									Drop us a note. 
 								</h2>
-								<form className="flex flex-col w-full" method="POST" data-netlify="true" name="contact" action="/success">
-									<div className="space-y-3">
-										<label className="flex flex-col">
-											<span className="text-xs tracking-tight font-medium uppercase text-gray-400 mb-1">
-												Name
-											</span>
-											<input
-												type="text"
-												className="rounded border-none focus:ring-0"
-												name="name"
-												maxLength={25}
-												minLength={3}
-												autoComplete="name"
-												required
-											/>
-										</label>
-										<label className="flex flex-col">
-											<span className="text-xs tracking-tight font-medium uppercase text-gray-400 mb-1">
-												Email
-											</span>
-											<input
-												type="text"
-												className="rounded border-none focus:ring-0"
-												name="email"
-												maxLength={25}
-												minLength={3}
-												autoComplete="email"
-												required
-											/>
-										</label>
-										<label className="flex flex-col">
-											<span className="text-xs tracking-tight font-medium uppercase text-gray-400 mb-1">
-												Message
-											</span>
-											<textarea
-												className="rounded border-none focus:ring-0"
-												name="message"
-												maxLength={2000}
-												minLength={3}
-												required
-											/>
-										</label>
-									</div>
-									<input
-										type="reset"
-										className="link cursor-pointer focus:ring rounded mr-auto mt-1 bg-transparent"
-										value="Clear"
-									/>
-									<input
-										type="submit"
-										className="ml-auto btn bg-blue-500 text-gray-700 font-bold rounded nightwind-prevent w-full px-auto mt-6 lg:mt-2 lg:w-auto"
-										value="Send"
-									/>
-								</form>
+								<ContactForm />
 							</div>
 						</div>
 					</div>
